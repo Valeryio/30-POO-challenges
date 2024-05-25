eb9ac41ae516d7743426bed33a14fbfc6936e0c3
Valeryio <tommagoma23@gmail.com>
2024-05-25 14:41:18 +0000

    Adding a template file

---
+++ tests/test_challenge_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+    This is the test module of the class ...
+"""
+
+import unittest
+from challenges.challenge_2 import ...
+
+
+class test_(unittes.TestCase):
+    pass
