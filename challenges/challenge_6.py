6495fd6257bed63ce28595b42d1543daed85e918
Valeryio <tommagoma23@gmail.com>
2024-06-16 20:34:42 +0000

    First draft of the tests for the challenge 6

--- challenges/challenge_6.py
+++ challenges/challenge_6.py
@@ -3,10 +3,22 @@
 # This is the module of the class Student
 
 class Student(Person):
-    """This is the class Student, a child of Person
+    """This is the class Student, a child of the class Person
 
      Attributes:
-        @
-        @
+        @level: (integer), the level of the scolarship of the
+                Child
     """
-    pass
+    def __init__(self, name, age, gender, level):
+        """This is the constructor calling the parent's constructor"""
+        super.__init__(name, age, gender)
+        self.level = level
+
+    @property
+    def level(self):
+        return self.__level
+
+    @level.setter
+    def level(self, level):
+        if type(level) is not str:
+            raise TypeError("The level should be an integer")
--- tests/test_challenge_6.py
+++ tests/test_challenge_6.py
@@ -3,8 +3,9 @@
 """
     This is the test module of the class Student
 """
-
+import sys
 import unittest
+from io import StringIO
 from challenges.challenge_6 import Student
 
 
@@ -12,11 +13,41 @@ class TestStudent(unittest.TestCase):
     """This is the test suite for the class Student
 
     Attributes:
-        @
+        @bigetty: (child object)
+        @pacman: (child object)
+        @student_list: (list), a list of child objects
     """
 
     def setUp(self):
-        """This test sets up the different attributes for all the tesst
+        """This test sets up the different attributes for all the test
         suite"""
-        pass
+        self.bigetty = Student("Bigetty", 20, "male", "Terminal")
+        self.pacman = Student("Erlyc", 38, "male", "Terminal")
+        self.student_list.add(self.bigetty)
+        self.student_list.add(self.pacman)
 
+    def test_attributes(self):
+        """This test checks if all the attributes are well saved"""
+        self.assertEqual(self.bigetty.age, 19)
+        self.assertEqual(self.bigetty.gender, "male")
+        self.assertEqual(self.bigetty.name, "Bigetty")
+        self.assertEqual(self.bigetty.level, "Terminal")
+
+        with self.assertRaises(TypeError) as context:
+            test_attribute = Student("Melda", 20,"female", 5)
+            self.assertEqual(str(context.content), "The level should be a integer")
+
+    def test_registered_students(self):
+        """This test checks the registered_students() method"""
+        result = f"[('Bigetty', 20, 'male', 'Terminal')"\
+                + f", ('Erlyc', 38, 'male', 'Terminal')]"
+        output_obj = StringIO()
+        sys.stdout = output_obj
+        
+        # Execute the method and listening to the output
+        self.bigetty.registered_students(self.student_list)
+
+        sys.stdout = sys.__stdout__
+
+        # Compares the right values
+        self.assertEqual(output_obj.getvalue(), result)
